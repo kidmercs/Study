7ca459be99c7dfd8d75b6a19289c42ee04e26b6c
mercs <103644600+kidmercs@users.noreply.github.com>
2026-05-29 20:54:12 +0000

    wer

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,16 +1,33 @@
-import express from "express";
+import express, { type Express } from "express";
 import cors from "cors";
+import pinoHttp from "pino-http";
 import router from "./routes";
+import { logger } from "./lib/logger";
 
-const app = express();
+const app: Express = express();
 
+app.use(
+  pinoHttp({
+    logger,
+    serializers: {
+      req(req) {
+        return {
+          id: req.id,
+          method: req.method,
+          url: req.url?.split("?")[0],
+        };
+      },
+      res(res) {
+        return {
+          statusCode: res.statusCode,
+        };
+      },
+    },
+  }),
+);
 app.use(cors());
 app.use(express.json());
-
-app.use((req, _res, next) => {
-  console.log(req.method, req.url);
-  next();
-});
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", router);
 
